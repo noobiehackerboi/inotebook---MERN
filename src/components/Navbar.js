41211a55e99f5e0033cc4c623169974f7bd90031
Divya <divyaratna1234@gmail.com>
2024-02-24 02:29:29 +0530

    Added navbar active feature using useLocation

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,7 @@
 import { Link, useLocation } from 'react-router-dom'
 
 const Navbar = () => {
-    let loaction = useLocation();
+    let location = useLocation();
     return (
         <nav className="navbar navbar-expand-lg bg-body-tertiary" data-bs-theme="dark">
             <div className="container-fluid">
@@ -12,10 +12,10 @@ const Navbar = () => {
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                         <li className="nav-item">
-                            <Link className={`nav-link ${loaction.pathname === '/' ? "active" : ""}`} aria-current="page" to="/">Home</Link>
+                            <Link className={`nav-link ${location.pathname === '/' ? "active" : ""}`} aria-current="page" to="/">Home</Link>
                         </li>
                         <li className="nav-item">
-                            <Link className={`nav-link ${loaction.pathname === '/about' ? "active" : ""}`} to="/about">About</Link>
+                            <Link className={`nav-link ${location.pathname === '/about' ? "active" : ""}`} to="/about">About</Link>
                         </li>
                     </ul>
                     <form className="d-flex" role="search">
